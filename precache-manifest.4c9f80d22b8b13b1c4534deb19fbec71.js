236780c147713d6452c3348c9a58e3f3111dd2d1
dghodse <ghodsednyaneshwar@gmail.com>
2021-06-11 09:55:58 +0530

    Updates

---
+++ precache-manifest.4c9f80d22b8b13b1c4534deb19fbec71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1054773f8b6d608388bba3bd4efc7176",
+    "url": "/react-birthday-reminder/index.html"
+  },
+  {
+    "revision": "f5b2c2918028cffa936b",
+    "url": "/react-birthday-reminder/static/css/main.0ab34b97.chunk.css"
+  },
+  {
+    "revision": "d1ccc1e6d556dbe2d9b7",
+    "url": "/react-birthday-reminder/static/js/2.d8a71053.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-birthday-reminder/static/js/2.d8a71053.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5b2c2918028cffa936b",
+    "url": "/react-birthday-reminder/static/js/main.920ff4c0.chunk.js"
+  },
+  {
+    "revision": "ca18d9339a309e58c265",
+    "url": "/react-birthday-reminder/static/js/runtime-main.4fe2f5be.js"
+  }
+]);
